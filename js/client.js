fdbd07f82b50d4a8a175581ad927b79be2dc17b0
Tanriverdi Nicolas <tanriverdi.nicolas@yahoo.fr>
2019-04-19 16:43:47 +0200

    make it better

--- js/client.js
+++ js/client.js
@@ -12,12 +12,12 @@ window.addEventListener('DOMContentLoaded', function(){
     monForm.onsubmit = function (event) {
     var limiteValue = monInput.value;
     if (monInput.value === "" && limiteValue.length < 8) {
-        event.preventDefault();
-       alert('Vous devez entrer un identifiant, ne pas dépasser 8 caractéres');
+    //     event.preventDefault();
+    //    alert('Vous devez entrer un identifiant, ne pas dépasser 8 caractéres');
     } else {
         
         maDivSupp.style.display = "none";
-        event.preventDefault();
+        // event.preventDefault();
 
     var websocketConnection = io('https://sniper-game.herokuapp.com/');
     // Je défini le port et l'adresse ip de connection
