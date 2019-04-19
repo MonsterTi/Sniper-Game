12029e3800469cab6c75024bfd62d6d66cd5ed47
Tanriverdi Nicolas <tanriverdi.nicolas@yahoo.fr>
2019-04-19 16:56:56 +0200

    make it better

--- js/client.js
+++ js/client.js
@@ -12,7 +12,7 @@ window.addEventListener('DOMContentLoaded', function(){
     monForm.onsubmit = function (event) {
     var limiteValue = monInput.value;
 
-    if (monInput.value === "" && limiteValue.length < 8) {
+    if (monInput.value === "" && limiteValue.length > 8) {
         event.preventDefault();
        alert('Vous devez entrer un identifiant, ne pas dépasser 8 caractéres');
     } else {
